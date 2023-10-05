3c3644f6077fdbf364696dcd902767b3c548b629
Samuel <sgeorge@apizr.fr>
2023-10-05 14:29:28 +0200

    exercice 2 to 8

--- src/Exercices/Stream/Main.java
+++ src/Exercices/Stream/Main.java
@@ -2,14 +2,15 @@ package Exercices.Stream;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Trader trader1 = new Trader("Abdallah", "Cambridge");
         Trader trader2 = new Trader("Audrey", "Milan");
-        Trader trader3 = new Trader("Corentin", "Paris");
-        Trader trader4 = new Trader("Tristan", "Paris");
+        Trader trader3 = new Trader("Corentin", "Cambridge");
+        Trader trader4 = new Trader("Tristan", "Cambridge");
 
         List<Transaction> transactions = Arrays.asList(new Transaction(trader1, 2011, 500), new Transaction(trader2, 2012, 300), new Transaction(trader3, 2012, 1000), new Transaction(trader4, 2011, 400));
 
@@ -31,6 +32,50 @@ public class Main {
 
         System.out.println("Exercice 3");
 
+        String noms = transactions.stream().map(t -> t.getTrader().getName()).sorted().collect(Collectors.toList()).toString();
 
+        System.out.println(noms);
+
+        System.out.println("Exercice 4");
+
+        Boolean cityEqualsMilan = transactions.stream().map(t -> t.getTrader().getCity()).anyMatch(c -> c.equals("Milan"));
+
+        System.out.println(cityEqualsMilan);
+
+        System.out.println("Exercice 5");
+
+        Optional<Integer> transactionLaPlusElevee = transactions.stream()
+                .map(t -> t.getValue())
+                .max(Integer::compareTo);
+
+        System.out.println(transactionLaPlusElevee);
+
+        System.out.println("Exercice 6");
+
+        List<Trader> cambridgeTraders = transactions.stream()
+                .map(t -> t.getTrader())
+                .filter(tr -> tr.getCity().equals("Cambridge"))
+                .sorted((t1, t2) -> t1.getName().compareTo(t2.getName())).collect(Collectors.toList());
+
+        System.out.println(cambridgeTraders);
+
+        System.out.println("Exercice 7");
+
+        List<Integer> getValuesFromCambridgeTraders = transactions.stream()
+                .filter(t -> t.getTrader().getCity().equals("Cambridge"))
+                .map(tr -> tr.getValue())
+                .collect(Collectors.toList());
+
+        System.out.println(getValuesFromCambridgeTraders);
+
+        System.out.println("Exercice 8");
+
+        Optional<Integer> transactionAvecLaPlusPetiteValue = transactions.stream().map(t -> t.getValue()).min(Integer::compareTo);
+
+        System.out.println(transactionAvecLaPlusPetiteValue);
+
+        System.out.println("Exercice 9");
+
+        // ?
     }
 }
